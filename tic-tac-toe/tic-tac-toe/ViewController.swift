a6305191a00f8db9722808acd909a7c3c553cea1
Eshan I <iyereshan@gmail.com>
2022-10-04 12:14:40 -0500

    Worked on Tic-Tac-Toe
    Co-Authored-By: nwilki <72949393+Cr1ms0nC0de@users.noreply.github.com>

--- tic-tac-toe/tic-tac-toe/ViewController.swift
+++ tic-tac-toe/tic-tac-toe/ViewController.swift
@@ -8,31 +8,47 @@
 import UIKit
 
 var currentBoard = ["","","","","","","","",""]
+var xTurn : Bool = true
 
 class ViewController: UIViewController {
-
-
+    @IBOutlet weak var turnIndicator: UILabel!
+    
+    
     @IBOutlet var allOutlets: [UIButton]!
- 
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func square1pressed(_ sender: Any) {
-        allOutlets[0].setTitle(currentBoard[0], for: .normal)
-        allOutlets[1].setTitle(currentBoard[1], for: .normal)
-        allOutlets[2].setTitle(currentBoard[2], for: .normal)
-        allOutlets[3].setTitle(currentBoard[3], for: .normal)
-        allOutlets[4].setTitle(currentBoard[4], for: .normal)
-        allOutlets[5].setTitle(currentBoard[5], for: .normal)
-        allOutlets[6].setTitle(currentBoard[6], for: .normal)
-        allOutlets[7].setTitle(currentBoard[7], for: .normal)
-        allOutlets[8].setTitle(currentBoard[8], for: .normal)
         
+        for i in 0...8
+        {
+            allOutlets[i].setTitle(currentBoard[i], for: .normal)
+            allOutlets[i].addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+            allOutlets[i].tag = i
+        }
+        
+        turnIndicator.text = "It is X's turn"
         
     }
     
-
+    
+    @objc func buttonPressed(_ sender: UIButton!)
+    {
+        var str = ""
+        
+        if xTurn
+        {
+            str = "X"
+        }
+        else
+        {
+            str = "O"
+        }
+        
+        allOutlets[sender.tag].setTitle(str, for: .normal)
+        
+    }
+    
+    
 }
 

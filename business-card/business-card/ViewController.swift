53ee0aa7ac6eb191bc01942271b5560da9de780a
Eshan I <iyereshan@gmail.com>
2022-10-26 10:53:22 -0500

    Business Card project finished

--- business-card/business-card/ViewController.swift
+++ business-card/business-card/ViewController.swift
@@ -12,21 +12,36 @@ import UIKit
 
 class ViewController: UIViewController {
     var picture = UIImageView()
-    var screenWidth : Double = 0
-    var screenHeight : Double = 0
+    let name = UILabel()
+    let jobTitle = UILabel()
+    let contactHider = UIButton()
+    let contactOne = UILabel()
+    let contactTwo = UILabel()
     
-
+    var screenWidth : Double = UIScreen.main.bounds.width
+    var screenHeight: Double = UIScreen.main.bounds.height
+    
+    var currentState = false
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        let screenBounds: CGRect = UIScreen.main.bounds
         
+        super.viewDidLoad()
+
         view.backgroundColor = UIColor.blue
         
-        screenWidth  = screenBounds.width
-        screenHeight = screenBounds.height
-        print("The Screen Width is \(screenWidth)")
-        print("The Screen Height is \(screenHeight)")
+        self.configureJobTitle()
+        self.configureProfilePic()
+        self.configureNameText()
+        self.configureContactHider()
+        self.configureContacts()
         
+        contactOne.isHidden = true
+        contactTwo.isHidden = true
+        
+        self.view = view
+    }
+    func configureProfilePic() // Adds a picture of my face
+    {
         picture.image = UIImage(named: "face.jpg")
         picture.frame = CGRect(x: screenWidth * 0.25, y: screenHeight/4, width: 200, height: 200)
         picture.layer.cornerRadius = picture.frame.width/2
@@ -35,30 +50,75 @@ class ViewController: UIViewController {
         picture.layer.borderColor = UIColor.white.cgColor
         view.addSubview(picture)
         
-        let name = UILabel(frame: CGRect(x: screenWidth * 0.03, y: screenHeight/3.5, width: 400, height: 400))
+    }
+    func configureNameText() // Adds my name
+    {
+        name.frame = CGRect(x: screenWidth * 0.03, y: screenHeight/3.5, width: 400, height: 400)
         name.textAlignment = .center
         name.textColor = .white
         name.font = UIFont.systemFont(ofSize: 40)
         name.text = "Eshan Iyer"
+        
         view.addSubview(name)
+    }
+    func configureJobTitle() // Adds my job title
+    {
+        jobTitle.frame = CGRect(x: screenWidth * 0.03, y: screenHeight/3, width: 400, height: 400)
+        jobTitle.textAlignment = .center
+        jobTitle.textColor = .white
+        jobTitle.font = UIFont.systemFont(ofSize: 25)
+        jobTitle.text = "Machine Learning Enthusiast"
         
-        let title = UILabel(frame: CGRect(x: screenWidth * 0.03, y: screenHeight/3, width: 400, height: 400))
-        title.textAlignment = .center
-        title.textColor = .white
-        title.font = UIFont.systemFont(ofSize: 25)
-        title.text = "Machine Learning Enthusiast"
-        view.addSubview(title)
+        view.addSubview(jobTitle)
         
-        let contactHider = UIButton(frame: CGRect(x: screenWidth * 0.03, y: screenHeight/3, width: 400, height: 400))
-        contactHider.setTitle("test", for: .normal)
+    }
+    func configureContactHider() // Adds button that allows you to hide the contact info
+    {
+        contactHider.frame = CGRect(x: screenWidth * 0.03, y: screenHeight/2.7, width: 400, height: 400)
+        contactHider.setTitle("Tap to show contact info", for: .normal)
         contactHider.titleLabel?.textAlignment = .center
-//        contactHider.titleLabel?.textColor = UIColor
+        contactHider.titleLabel?.textColor = UIColor.white
+        contactHider.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
         
         view.addSubview(contactHider)
-        
-        self.view = view
     }
-
-
+    func configureContacts() // Adds the contacts themselves
+    {
+        contactOne.frame = CGRect(x: screenWidth * 0.03, y: screenHeight/1.59, width: 400, height: 25)
+        contactOne.textAlignment = .center
+        contactOne.text = "Phone: +1 (972) 439-4254"
+        contactOne.backgroundColor = .white
+        contactOne.layer.cornerRadius = 10
+        contactOne.layer.masksToBounds = true
+        
+        contactTwo.frame = CGRect(x: screenWidth * 0.03, y: screenHeight/1.5, width: 400, height: 25)
+        contactTwo.textAlignment = .center
+        contactTwo.text = "Email: admin@eshaniyer.tech"
+        contactTwo.backgroundColor = .white
+        contactTwo.layer.cornerRadius = 10
+        contactTwo.layer.masksToBounds = true
+        
+        view.addSubview(contactOne)
+        view.addSubview(contactTwo)
+    }
+    @objc func buttonPressed(_ sender: UIButton!) { // Function that is called when the button is pressed
+        
+        if currentState == false
+        {
+            contactHider.setTitle("Tap to hide contact info", for: .normal)
+            contactOne.isHidden = false
+            contactTwo.isHidden = false
+            currentState = true
+        }
+        else
+        {
+            contactHider.setTitle("Tap to show contact info", for: .normal)
+            
+            contactOne.isHidden = true
+            contactTwo.isHidden = true
+            
+            currentState = false
+        }
+        
+    }
 }
-

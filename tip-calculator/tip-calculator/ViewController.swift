f1158fb4e354b55bc8a8f6f6f21f5b80dd05dee6
Eshan I <iyereshan@gmail.com>
2022-12-02 12:14:31 -0600

    Tip Calculator UI Finished, some work needed

--- tip-calculator/tip-calculator/ViewController.swift
+++ tip-calculator/tip-calculator/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountInput: UITextField!
+    
+    @IBOutlet weak var totalTip: UILabel!
+    
+    @IBOutlet weak var tipSelection: UISegmentedControl!
+    
+    @IBOutlet weak var totalBill: UILabel!
+    
+    @IBOutlet weak var otherEntry: UITextField!
+    
+    var billTotal : Double = 0
+    var tipTotal : Double = 0
+    var userTipPercent : Double = 0
+    var billAmount : Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

a77d2f4b553a4d6d9d4a6475cb45255945e687c0
Eshan I <iyereshan@gmail.com>
2022-12-20 12:15:36 -0600

    Finished AddOne

--- add-one/addOne/ViewController.swift
+++ add-one/addOne/ViewController.swift
@@ -17,36 +17,138 @@ class ViewController: UIViewController {
     @IBOutlet weak var userInput: UITextField!
     
     var timer = Timer() // Timer for generation
+    
+    var time : Int = 60
+    
+    var score = 0
+    
+    var currentNumber : Int = 0
+    
     override func viewDidLoad() {
         setNewNumber()
+        
+        timeLabel.text = "60"
+        
+        scoreLabel.text = String(score)
         
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.jpg")!)
+        
+        userInput.addTarget(self, action: #selector(ViewController.userInputChanged(_:)), for: .editingChanged)
+        
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
     }
-    @objc func timerFunction(_ sender: UIButton!) {
-         timer.invalidate()
-         
-         // 1 second until next question
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerFunction2), userInfo: nil, repeats: true)
-         
-                  
-         
-        timeLabel.text = timer.
-         
-         }
-     @objc
     func generateNumber() -> String {
-        var digits = 4
+        let digits = 4
         var number = String()
         for _ in 1...digits {
-           number += "\(Int.random(in: 1...9))"
+            number += "\(Int.random(in: 1...9))"
         }
+        currentNumber = Int(number)!
         return number
     }
     func setNewNumber(){
         number.text = generateNumber()
+    }
+    @objc func timerFunction() {
+        timer.invalidate()
+        
+        time -= 1
+        if time == 0
+        {
+            displayAlert()
+        }
+        else{
+            
+            timeLabel.text = String(format: "%02d", time)
+            
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+        }
+    }
+    func displayAlert()
+    {
+        let dialogMessage = UIAlertController(title: "Game Over!", message: "Score: \(score)", preferredStyle: .alert)
+        
+        dialogMessage.addAction(UIAlertAction(title: "Click to restart", style: .default, handler: {_ in self.alertPressed()}))
+        
+        self.present(dialogMessage, animated: true, completion: nil)
+        
+        
+    }
+    func alertPressed()
+    {
+        // Resets everything to original values
+        
+        time = 60
+        
+        setNewNumber()
+        
+        userInput.text = ""
+        timeLabel.text = String(time)
+        
+        scoreLabel.text = "0"
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+    }
+    func getAnswer() -> String
+    {
+        var correctNumber: String = ""
+        let splitInputText = Array(number.text!)
+        var integerArray = [0, 0, 0, 0]
+        
+        for i in 0...3
+        {
+            let temp = splitInputText[i]
+            
+            integerArray[i] = Int(String(temp))!
+            integerArray[i] += 1
+            correctNumber += String(integerArray[i])
+        }
+        return correctNumber
+        
+    }
+    func checkNumber()
+    {
+        let currentInputText = userInput.text!
+        
+        if currentInputText == getAnswer()
+        {
+            // User inputted correct answer
+            score += 1
+            
+            scoreLabel.text = String(score)
+            
+            userInput.text = ""
+            
+            setNewNumber()
+            
+            print("point")
+            
+        }
+        else
+        {
+            // User inputted wrong number
+            score -= 1
+            scoreLabel.text = String(score)
+            
+            userInput.text = ""
+            
+            print("wrong")
+        }
+    }
+    @objc func userInputChanged(_ textField: UITextField) {
+        let currentInput = userInput.text
+        
+        if currentInput!.count != 4
+        {
+            print("Invalid input")
+        }
+        else
+        {
+            checkNumber()
+        }
+        
+        
     }
 }
 

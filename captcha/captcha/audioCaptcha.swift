aef01b197d043deb416fb56a5a78f567019708bb
Eshan I <iyereshan@gmail.com>
2023-03-30 12:16:34 -0500

    99% Done with captcha

--- captcha/captcha/audioCaptcha.swift
+++ captcha/captcha/audioCaptcha.swift
@@ -9,8 +9,10 @@ import UIKit
 import AVFoundation
 
 var soundList : [String] = []
-var soundNameList : [String] = []
 var currentAnswer : String = ""
+var currentAnswerName : String = ""
+var randomIndex : Int = 0
+var incorrectAnswerCount : Int = 0
 
 class audioCaptcha: UIViewController {
     var player: AVAudioPlayer!
@@ -34,28 +36,41 @@ class audioCaptcha: UIViewController {
         } catch {
             print("\nError\n")
         }
-        for i in soundList
-        {
-            var j : String = i
-            var index = j.firstIndex(of: "_")!
-            j = j.substring(from: index)
-            index = j.firstIndex(of: ".")!
-            j = j.substring(to: index)
-            j.removeFirst()
-            
-            soundNameList.append(j)
-        }
+        
+        
         for i in 0...3
         {
-            answerChoices[i].addTarget(self, action: #selector(answerChoicePressed(_:)), for: .touchUpInside)
-            answerChoices[i].backgroundColor = .blue
+            answerChoiceButtons[i].addTarget(self, action: #selector(answerChoicePressed(_:)), for: .touchUpInside)
+            answerChoiceButtons[i].backgroundColor = .blue
         }
+        assignAnswer()
+        assignButtons(flag: true)
         
     }
-    
+    func wavRemover(wavName : String) -> String
+    {
+        var j : String = wavName
+        var index = j.firstIndex(of: ".")!
+        j = j.substring(to: index)
+        
+        return j
+        
+    }
+    func fileToName(file:String) -> String
+    {
+        var j : String = file
+        var index = j.firstIndex(of: "_")!
+        j = j.substring(from: index)
+        index = j.firstIndex(of: ".")!
+        j = j.substring(to: index)
+        j.removeFirst()
+        
+        return j
+        
+    }
     func playSound(name:String){
-        print("\nname\n")
-        let url = Bundle.main.url(forResource: "/Sounds/\(name)", withExtension: "wav")
+        let soundDirectory = "/Sounds/\(name)"
+        let url = Bundle.main.url(forResource: soundDirectory, withExtension: "wav")
         
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
@@ -66,27 +81,83 @@ class audioCaptcha: UIViewController {
         t.removeLast(4)
         return t
     }
-    func assignButtons()
+    func assignButtons(flag:Bool)
     {
-        var j = 1
+        var j = 0
         for i in answerChoiceButtons
         {
-            let text = soundNameList[j]
+            let text = fileToName(file: soundList[j])
             i.setTitle(text, for: .normal)
             j+=1
         }
+        print("\nJ value: \(j)\n")
+        if !flag
+        {
+            soundList.removeFirst(4)
+        }
+
+        print("\nUpdated Sound List: \(soundList)\n")
     }
     func assignAnswer()
     {
-        var lst : [String]= soundNameList
-        lst.shuffle()
+        randomIndex = Int.random(in: 0 ... 3)
+        correctAnswer = soundList[randomIndex]
         
     }
-       @objc func answerChoicePressed(_ sender: UIButton!){
-        let pickedAnswer : String = sender.currentTitle
+    @objc func answerChoicePressed(_ sender: CaptchaButton!){
+        let pickedAnswer : String = sender.currentTitle!
         
-       }
+        print("\nSender title: \(pickedAnswer)\n")
+        print("\nCorrect Answer: \(fileToName(file: correctAnswer))\n")
+        
+        let validity = checkAnswer(answer: pickedAnswer)
+        if !validity
+        {
+            sender.shake()
+        }
+        transition(answerValidity: validity)
+        
+       
+        
+    }
     
-    
-    
+    @IBAction func playButtonPressed(_ sender: Any) {
+        playSound(name: wavRemover(wavName: correctAnswer))
+    }
+    func checkAnswer(answer : String) -> Bool
+    {
+        if answer == fileToName(file: correctAnswer)
+        {
+            print("\ncorrect audio pressed\n")
+            return true
+        }
+        else
+        {
+            print("\nincorrect audio pressed\n")
+            incorrectAnswers += 1
+            return false
+        }
+    }
+    func transition(answerValidity : Bool)
+    {
+        if answerValidity && incorrectAnswerCount < 2
+        {
+            // Text Captcha Passed
+            incorrectAnswerCount = 0
+            performSegue(withIdentifier: "notARobot", sender: nil)
+            
+        }
+        else if !answerValidity && incorrectAnswerCount == 2
+        {
+            // Text Captcha Failed
+            incorrectAnswerCount = 0
+            performSegue(withIdentifier: "tooWrong", sender: nil)
+        }
+        else
+        {
+            // First Attempt Failed
+            assignAnswer()
+            assignButtons(flag: false)
+        }
+    }
 }

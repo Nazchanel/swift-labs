6b2f1d7d2784a92d383bf0db37e9ef7e293ca0f9
Eshan I <iyereshan@gmail.com>
2023-04-13 11:13:21 -0500

    Worldle progress

--- Wordle/Wordle/ViewController.swift
+++ Wordle/Wordle/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import Foundation
+
+var correctWord : String = ""
 
 class ViewController: UIViewController {
-
+    var allWords = [String]()
+    let wordLength : Int = 5
+    let guessAmount : Int = 6
+    
+    @IBOutlet weak var deadLetterLabel: UILabel!
+    
+    @IBOutlet var allLetters: [UILabel]!
+    
+    @IBOutlet weak var wordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+         
+        if let startWordsURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+
+        print("\(allWords.count)\n")
+        
+        wordSelection()
+
     }
 
+    @IBAction func wordEntered(_ sender: Any) {
+        let wordEntered = wordField.text!.uppercased()
+        
+        wordField.text = ""
 
+        print(correctWord)
+        
+        if wordEntered == correctWord
+        {
+            print("correct")
+        }
+        
+        
+    }
+    
+    
+    func wordSelection()
+    {
+        correctWord = allWords.randomElement()!
+        if correctWord.count != wordLength
+        {
+            wordSelection()
+        }
+        
+    }
+    
 }
 

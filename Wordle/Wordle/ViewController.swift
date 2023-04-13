512218b8b325db3fd5da28bcafafcab0428e8e54
Eshan I <iyereshan@gmail.com>
2023-04-13 12:15:39 -0500

    Almost done with Wordle

--- Wordle/Wordle/ViewController.swift
+++ Wordle/Wordle/ViewController.swift
@@ -12,6 +12,9 @@ var correctWord : String = ""
 
 class ViewController: UIViewController {
     var allWords = [String]()
+    var colorList = []
+    var currentLabelIndex : Int = 0
+    
     let wordLength : Int = 5
     let guessAmount : Int = 6
     
@@ -20,17 +23,16 @@ class ViewController: UIViewController {
     @IBOutlet var allLetters: [UILabel]!
     
     @IBOutlet weak var wordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-         
+    
         if let startWordsURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
             }
         }
 
-        print("\(allWords.count)\n")
         
         wordSelection()
 
@@ -40,15 +42,11 @@ class ViewController: UIViewController {
         let wordEntered = wordField.text!.uppercased()
         
         wordField.text = ""
-
-        print(correctWord)
         
-        if wordEntered == correctWord
+        if wordEntered.count == wordLength
         {
-            print("correct")
+            wordCheck(guess: wordEntered)
         }
-        
-        
     }
     
     
@@ -61,6 +59,63 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    func wordCheck(guess : String)
+    {
+        var guessArray = guess.split(separator: "")
+        let correctWordArray = correctWord.split(separator: "")
+        
+        // 1 is green, 2 is yellow, and 0 is grey
+        var tempcolorList : [Int] = [0, 0, 0, 0, 0]
+        
+        print("Correct Word: \(correctWord)")
+        print("Guess Word: \(guess)")
+        
+        // Green Checker
+        for i in [Int](0...guessArray.count-1)
+        {
+            if guessArray[i] == correctWordArray[i]
+            {
+            
+                colorList[i] = 1
+                guessArray[i] = "&" // Prevents the yellow checker for mistaking a green for a yellow
+                
+            }
+        }
+        
+        // Yellow Checker
+        for i in [Int](0...guessArray.count-1)
+        {
+            let index = correctWordArray.firstIndex(of: guessArray[i])
+            if index != nil
+            {
+                colorList[i] = 2
+            }
+
+        }
+        setLabels(colorList: colorList)
+        
+    }
+    func setLabels(colorList : [Int])
+    {
+        
+        for i in [Int](currentLabelIndex...currentLabelIndex+4)
+        {
+            if colorList[i] == 1
+            {
+                allLetters[i].backgroundColor = #colorLiteral(red: 0.07419168204, green: 0.8280491829, blue: 0, alpha: 1)
+            }
+            if colorList[i] == 2
+            {
+                allLetters[i].backgroundColor = #colorLiteral(red: 0.8645705581, green: 0.8751729131, blue: 0, alpha: 1)
+            }
+            
+            
+        }
+        currentLabelIndex += 5
+        
+    }
+    
     
 }
 

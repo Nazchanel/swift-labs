f558053a114fb0a0ed73f70e69ba210c03b8d572
Eshan I <iyereshan@gmail.com>
2023-04-28 12:15:08 -0500

    Major Progress on Mynance

---
+++ mynance/mynance/PurchaseTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PurchaseTableViewCell.swift
+//  mynance
+//
+//  Created by Iyer, Eshan R on 4/28/23.
+//
+
+import UIKit
+
+class PurchaseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- mynance/mynance/View Controllers/NewTransactionViewController.swift
+++ mynance/mynance/View Controllers/NewTransactionViewController.swift
@@ -9,17 +9,29 @@ import UIKit
 
 let paymentTypes = ["Income", "Utilities", "Groceries", "Other Purchase"]
 
-class NewTransactionViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    @IBOutlet weak var paymentPicker: UIPickerView!
+var moneyEntered : String = ""
 
+var descriptionEntered : String = ""
+
+var selectedPaymentType : String = ""
+
+class NewTransactionViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let defaults = UserDefaults.standard
+    
+    var users: [String: String] = [:]
+    
+    @IBOutlet weak var paymentPicker: UIPickerView!
+    
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var moneyField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.paymentPicker.delegate = self
         self.paymentPicker.dataSource = self
     }
-    
-    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -30,17 +42,66 @@ class NewTransactionViewController: UIViewController, UIPickerViewDelegate, UIPi
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        selectedPaymentType = paymentTypes[row]
+        
+        print(paymentTypes[row])
+        
         return paymentTypes[row]
         
     }
     
+    @IBAction func descriptionFieldChanged(_ sender: Any) {
+        descriptionEntered = descriptionField.text!
+        
+        print(descriptionField.text!)
+    }
+    @IBAction func moneyFieldChanged(_ sender: Any) {
+        
+        if let amountString = moneyField.text?.currencyInputFormatting() {
+            moneyField.text = amountString
+        }
+        moneyEntered = moneyField.text!.replacingOccurrences(of: "$", with: "")
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-
+    
+    @IBAction func saveTest(_ sender: Any) {
+        // Do after save is pressed
+        
+    }
     
     
 }
+extension String {
+    
+    // formatting text for currency textField
+    func currencyInputFormatting() -> String {
+        
+        var number: NSNumber!
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currencyAccounting
+        formatter.currencySymbol = "$"
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        
+        var amountWithPrefix = self
+        
+        // remove from String: "$", ".", ","
+        let regex = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
+        amountWithPrefix = regex.stringByReplacingMatches(in: amountWithPrefix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count), withTemplate: "")
+        
+        let double = (amountWithPrefix as NSString).doubleValue
+        number = NSNumber(value: (double / 100))
+        
+        // if first number is 0 or all numbers were deleted
+        guard number != 0 as NSNumber else {
+            return ""
+        }
+        
+        return formatter.string(from: number)!
+    }
+}
+
